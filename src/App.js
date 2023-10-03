e8cfe710a6eccea3ade495e4479f20bcf7d0f740
vatan-prakash <90102762+vatan-prakash@users.noreply.github.com>
2023-10-03 22:17:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-
 import './App.css';
-import Accordion from './Components/About';
 import Navbar from './Components/Navbar';
-import Textform from './Components/Textform';
+import TextForm from './Components/Textform';
+import About from './Components/About';
+import React, { useState } from 'react';
+import Alert from './Components/Alert';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-       <>
-        <Navbar title="Textutis" />
-        <div className="container">
-        <Textform heading="Enter the text to analyze"/> 
-        <Accordion/> 
-        </div>
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
 
-       </>
-    </div>
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert('Dark mode has been enabled', 'success');
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light mode has been enabled', 'success');
+    }
+  };
+
+  return (
+    <Router>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Routes>
+          <Route path="/about" element={<About mode={mode} />} />
+          <Route path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
